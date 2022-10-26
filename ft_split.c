f379eedaba7f1a0844b1fecc3448b4fe12dc6266
Abdelhadi Amhamdi <aamhamdi@e2r13p1.1337.ma>
2022-10-26 21:35:29 +0100

    next commit

--- ft_split.c
+++ ft_split.c
@@ -6,22 +6,22 @@
 /*   By: aamhamdi <aamhamdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 15:31:59 by aamhamdi          #+#    #+#             */
-/*   Updated: 2022/10/26 21:02:31 by aamhamdi         ###   ########.fr       */
+/*   Updated: 2022/10/26 21:31:59 by aamhamdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	get_tabs_sum(const char *s1, char c)
+static int	get_tabs_sum(const char *src, char c)
 {
 	int	i;
 	int	size;
 
 	i = 0;
 	size = 0;
-	while (s1[i])
+	while (src[i])
 	{
-		if ((s1[i] == c && s1[i - 1] != c && i != 0) || s1[i + 1] == '\0')
+		if ((src[i] == c && src[i - 1] != c && i != 0) || src[i + 1] == '\0')
 			size++;
 		i++;
 	}
@@ -39,31 +39,31 @@ static char	**ft_free(char **tabs, int index)
 	return (0);
 }
 
-char	**ft_split(const char *s, char c)
+char	**ft_split(const char *src, char c)
 {
-	char	**str;
+	char	**tabs;
 	size_t	i;
 	size_t	len;
 
-	if (!s)
+	if (!src)
 		return (NULL);
-	str = (char **)malloc(sizeof(char *) * (get_tabs_sum(s, c) + 1));
-	if (!str)
+	tabs = (char **)malloc(sizeof(char *) * (get_tabs_sum(src, c) + 1));
+	if (!tabs)
 		return (0);
 	i = 0;
-	while (*s)
+	while (*src)
 	{
-		while (*s && *s == c)
-			s++;
+		while (*src && *src == c)
+			src++;
 		len = 0;
-		while (s[len] && s[len] != c)
+		while (src[len] && src[len] != c)
 			len++;
 		if (len != 0)
-			str[i++] = ft_substr(s, 0, len);
-		if (len != 0 && !str[i - 1])
-			return (ft_free(str, i - 1));
-		s += len;
+			tabs[i++] = ft_substr(src, 0, len);
+		if (len != 0 && !tabs[i - 1])
+			return (ft_free(tabs, i - 1));
+		src += len;
 	}
-	str[i] = NULL;
-	return (str);
+	tabs[i] = NULL;
+	return (tabs);
 }
